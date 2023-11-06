049bb9f2f47175e8b761226b3c0ae009d432b682
jmillana <jordi@marimon.tech>
2023-11-06 18:46:58 +0100

    Implemented window build tutorial

---
+++ src/tutorial_1/window.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+const log = std.log.scoped(.Engine);
+
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
+
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("Failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    const window = glfw.Window.create(640, 480, "zig opentl test", null, null, .{
+        .opengl_profile = .opengl_core_profile,
+        .context_version_major = 4,
+        .context_version_minor = 6,
+    }) orelse {
+        std.log.err("Failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
+
+    // Wait for the user to close the window.
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+
+        gl.clearColor(1, 0, 1, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        window.swapBuffers();
+    }
+}
